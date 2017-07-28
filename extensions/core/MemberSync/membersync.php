803f61b3af833cbb3975c2114a618f19c972a467
Ahmad <ahmadelbardan@hotmail.de>
2017-07-28 21:30:41 +0200

    Minor changes...

--- extensions/core/MemberSync/membersync.php
+++ extensions/core/MemberSync/membersync.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @brief		Member Sync
- * @author		<a href='http://www.invisionpower.com'>Invision Power Services, Inc.</a>
- * @copyright	(c) 2001 - 2016 Invision Power Services, Inc.
- * @license		http://www.invisionpower.com/legal/standards/
- * @package		IPS Community Suite
- * @subpackage	Discord Integration
- * @since		29 Jan 2017
- * @version		SVN_VERSION_NUMBER
+ * @brief            Member Sync
+ * @author           <a href='http://www.invisionpower.com'>Invision Power Services, Inc.</a>
+ * @copyright    (c) 2001 - 2016 Invision Power Services, Inc.
+ * @license          http://www.invisionpower.com/legal/standards/
+ * @package          IPS Community Suite
+ * @subpackage       Discord Integration
+ * @since            29 Jan 2017
+ * @version          SVN_VERSION_NUMBER
  */
 
 namespace IPS\discord\extensions\core\MemberSync;
@@ -27,19 +27,22 @@ class _membersync
     /**
      * Member account has been updated
      *
-     * @param	$member		\IPS\Member	Member updating profile
-     * @param	$changes	array		The changes
-     * @return	void
+     * @param    $member        \IPS\Member    Member updating profile
+     * @param    $changes       array        The changes
+     * @return    void
      */
     public function onProfileUpdate( \IPS\Member $member, $changes )
     {
         /* Determine whether group(s) has been changed */
         if ( isset( $changes['member_group_id'] ) || isset( $changes['mgroup_others'] ) )
         {
-            try {
+            try
+            {
                 $guildMember = new \IPS\discord\Api\GuildMember;
                 $guildMember->update( $member, $changes );
-            } catch ( \Exception $e ) {
+            }
+            catch ( \Exception $e )
+            {
                 /* Ignore, can be re-synced. */
             }
         }
@@ -48,40 +51,61 @@ class _membersync
     /**
      * Member is flagged as spammer
      *
-     * @param	$member	\IPS\Member	The member
-     * @return	void
+     * @param    $member    \IPS\Member    The member
+     * @return    void
      */
     public function onSetAsSpammer( $member )
     {
-        if ( \IPS\Settings::i()->discord_sync_bans )
+        try
         {
-            $guildMember = new \IPS\discord\Api\GuildMember;
-            $guildMember->modifyBanState( $member );
+            if ( \IPS\Settings::i()->discord_sync_bans )
+            {
+                $guildMember = new \IPS\discord\Api\GuildMember;
+                $guildMember->modifyBanState( $member );
+            }
+        }
+        catch ( \Exception $e )
+        {
+            \IPS\Log::log( $e, 'discord_member_sync' );
         }
     }
 
     /**
      * Member is merged with another member
      *
-     * @param	\IPS\Member	$member		Member being kept
-     * @param	\IPS\Member	$member2	Member being removed
-     * @return	void
+     * @param    \IPS\Member $member  Member being kept
+     * @param    \IPS\Member $member2 Member being removed
+     * @return    void
      */
     public function onMerge( $member, $member2 )
     {
-        $guildMember = new \IPS\discord\Api\GuildMember;
-        $guildMember->remove( $member2 );
+        try
+        {
+            $guildMember = new \IPS\discord\Api\GuildMember;
+            $guildMember->remove( $member2 );
+        }
+        catch ( \Exception $e )
+        {
+            \IPS\Log::log( $e, 'discord_member_sync' );
+        }
     }
 
     /**
      * Member is deleted
      *
-     * @param	$member	\IPS\Member	The member
-     * @return	void
+     * @param    $member    \IPS\Member    The member
+     * @return    void
      */
     public function onDelete( $member )
     {
-        $guildMember = new \IPS\discord\Api\GuildMember;
-        $guildMember->remove( $member );
+        try
+        {
+            $guildMember = new \IPS\discord\Api\GuildMember;
+            $guildMember->remove( $member );
+        }
+        catch ( \Exception $e )
+        {
+            \IPS\Log::log( $e, 'discord_member_sync' );
+        }
     }
 }
